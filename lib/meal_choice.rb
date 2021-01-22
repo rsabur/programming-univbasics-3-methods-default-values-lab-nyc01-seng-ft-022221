12935d3adc1f3e371129bd37e45ac7180d752aca
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-01-22 23:33:28 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,8 @@
-def meal_choice(protein = "chicken", veg1, veg2)
+def meal_choice(protein = "tofu", veg1, veg2)
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  return "A plate of #{protein} with #{veg1} and #{veg2}"
 
 end
 
-meal_choice("potatos", "peas")
+meal_choice("chicken", "potatos", "peas")
