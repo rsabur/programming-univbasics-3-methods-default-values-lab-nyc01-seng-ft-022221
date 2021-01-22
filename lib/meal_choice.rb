44851aa6c1e1dab559a05ea0feb212986124729c
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-01-22 23:58:55 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,4 +4,4 @@ def meal_choice(veg1, veg2, protein = "tofu")
   return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice("String beans", "carrots")
+meal_choice("chicken", "String beans", "carrots")
